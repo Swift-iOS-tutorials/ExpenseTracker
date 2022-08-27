090f4a4d62fa8cbb9a65166e1c3480d7cbc2db23
Dennis Shar <dennis.shar@gmail.com>
2022-08-27 22:07:40 +0300

    add resent transaction and transaction list

--- ExpenseTracker/ExpenseTrackerApp.swift
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -9,9 +9,13 @@ import SwiftUI
 
 @main
 struct ExpenseTrackerApp: App {
+    // Decalare the view model here in order to make it run as soon as the app is up
+    @StateObject var transactionsListVM = TransactionListViewModel()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(transactionsListVM)
         }
     }
 }
--- ExpenseTracker/ViewModels/TransactionListViewModel.swift
+++ ExpenseTracker/ViewModels/TransactionListViewModel.swift
@@ -6,7 +6,58 @@
 //
 
 import Foundation
+import Combine
 
-final class TransactionListViewModel{
+typealias TransactionGroup = [String: [Transaction]]
+
+
+
+// Observable object turn every class into a pblisher and can notify about chnages in it to the view
+final class TransactionListViewModel: ObservableObject{
+    @Published var transactions: [Transaction] = []
     
+    private var cancellables = Set<AnyCancellable>()
+    
+    init (){
+        getTrabsactions()
+    }
+    
+    func getTrabsactions(){
+        guard let url = URL (string: "https://designcode.io/data/transactions.json") else {
+            print ("Invalid url")
+            return
+        
+        }
+        
+        URLSession.shared.dataTaskPublisher(for: url)
+            .tryMap { (data, responce) -> Data in
+                guard let httpResponce = responce as? HTTPURLResponse, httpResponce.statusCode == 200 else {
+                    dump(responce)
+                    throw URLError(.badServerResponse)
+                }
+                return data
+            }
+            .decode(type: [Transaction].self, decoder: JSONDecoder())
+            .receive(on: DispatchQueue.main)
+            .sink { completion in
+                switch completion {
+                case .failure(let error):
+                    print("Error fetching transactions:", error.localizedDescription)
+                case .finished:
+                    print("Finished fetching transaction")
+                }
+            } receiveValue: { [weak self] result in
+                self?.transactions = result
+                dump(self?.transactions) // dup is same as print 
+            }
+            .store(in: &cancellables)
+
+    }
+    
+    func groupTransactionByMOnth() -> TransactionGroup {
+        guard !transactions.isEmpty else { return [:] }// if emtpy return empty dic
+        
+        let groupedTransactions = TransactionGroup(grouping: transactions)  { $0.month }
+        return groupedTransactions
+   }
 }
